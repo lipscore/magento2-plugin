92d876ce26b43b2738bcc5bcf7140f2bfbd80f5a
Kamil Maliszewski <k.maliszewski92@icloud.com>
2023-03-08 12:47:09 +0100

    Add support for select in Brand attribute (#13)
    * Update Brand.php
    
    Add full support for `select` attributes in Brand attribute.
    
    * Fixes after code review
    
    * Fixes after CR

--- Model/System/Config/Source/Product/Brand.php
+++ Model/System/Config/Source/Product/Brand.php
@@ -2,19 +2,39 @@
 
 namespace Lipscore\RatingsReviews\Model\System\Config\Source\Product;
 
-use \Magento\Framework\Data\Collection;
+use Magento\Framework\Data\Collection;
+use Magento\Catalog\Model\Product;
 
 class Brand implements \Magento\Framework\Option\ArrayInterface
 {
-    protected $eavConfig;
+    /**
+     * @var \Magento\Eav\Model\ResourceModel\Entity\Attribute\CollectionFactory
+     */
+    protected $attributeFactory;
+
+    /**
+     * @var \Magento\Eav\Model\Entity\Type
+     */
+    protected $type;
+
+    /**
+     * @var \Lipscore\RatingsReviews\Model\Logger
+     */
     protected $logger;
 
+    /**
+     * @param \Lipscore\RatingsReviews\Model\Logger $logger
+     * @param \Magento\Eav\Model\ResourceModel\Entity\Attribute\CollectionFactory $attributeFactory
+     * @param \Magento\Eav\Model\Entity\Type $type
+     */
     public function __construct(
         \Lipscore\RatingsReviews\Model\Logger $logger,
-        \Magento\Eav\Model\Config $eavConfig
+        \Magento\Eav\Model\ResourceModel\Entity\Attribute\CollectionFactory $attributeFactory,
+        \Magento\Eav\Model\Entity\Type $type
     ) {
-        $this->eavConfig = $eavConfig;
-        $this->logger    = $logger;
+        $this->logger           = $logger;
+        $this->attributeFactory = $attributeFactory;
+        $this->type             = $type;
     }
 
     /**
@@ -32,8 +52,8 @@ class Brand implements \Magento\Framework\Option\ArrayInterface
             $attrs = $this->findAttrs();
             foreach ($attrs as $attr) {
                 $options[] = [
-                    'value' => $attr->getAttributeCode(),
-                    'label' => $attr->getStoreLabel()
+                    'value' => $attr['code'],
+                    'label' => $attr['label']
                 ];
             }
         } catch (\Exception $e) {
@@ -43,16 +63,24 @@ class Brand implements \Magento\Framework\Option\ArrayInterface
         return $options;
     }
 
+     /**
+     * Find attributes to render
+     * 
+     * @return array
+     */
     protected function findAttrs()
     {
         $attrs = [];
 
-        $collection = $this->eavConfig
-            ->getEntityType(\Magento\Catalog\Model\Product::ENTITY)
-            ->getAttributeCollection()
-            ->addFieldToFilter('backend_type', ['in' => ['varchar', 'text']])
+        $entityTypeId = $this->type->loadByCode(Product::ENTITY)->getId();
+        $collection = $this->attributeFactory->create()
+            ->removeAllFieldsFromSelect()
+            ->addFieldToSelect('attribute_code', 'code')
+            ->addFieldToSelect('frontend_label', 'label')
+            ->addFieldToFilter('entity_type_id', ['eq' => $entityTypeId])
+            ->addFieldToFilter('backend_type', ['in' => ['varchar', 'text', 'int']])
             ->addFieldToFilter('frontend_input', ['in' => ['text', 'textarea', 'select']])
-            ->addOrder('frontend_label', Collection::SORT_ORDER_ASC);
+            ->setOrder('frontend_label', Collection::SORT_ORDER_ASC);
 
         if ($collection->getSize() > 0) {
             return $collection->getItems();
