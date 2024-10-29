9c8e5a0cfde9fef93091ed1b367c7324662fe670
Adam <github@lipscore.com>
2024-10-29 11:33:05 +0100

    Fix product name in store language

--- Helper/Reminder.php
+++ Helper/Reminder.php
@@ -3,6 +3,7 @@
 namespace Lipscore\RatingsReviews\Helper;
 
 use Lipscore\RatingsReviews\Helper\AbstractHelper;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Sales\Model\Order;
 use Magento\Framework\UrlInterface;
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
@@ -11,6 +12,7 @@ class Reminder extends AbstractHelper
 {
     protected $ruleFactory;
     protected $productFactory;
+    protected $productRepository;
     protected $productHelper;
     protected $localeHelper;
     protected $couponHelper;
@@ -23,6 +25,7 @@ class Reminder extends AbstractHelper
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\SalesRule\Model\RuleFactory $ruleFactory,
         \Magento\Catalog\Model\ProductFactory $productFactory,
+        ProductRepositoryInterface $productRepository,
         \Lipscore\RatingsReviews\Helper\ProductFactory $productHelperFactory,
         \Lipscore\RatingsReviews\Helper\LocaleFactory $localeHelperFactory,
         \Lipscore\RatingsReviews\Helper\CouponFactory $couponHelperFactory,
@@ -32,6 +35,7 @@ class Reminder extends AbstractHelper
         parent::__construct($logger, $config, $storeManager);
 
         $this->ruleFactory       = $ruleFactory;
+        $this->productRepository = $productRepository;
         $this->productFactory    = $productFactory;
         $this->productTypeHelper = $productTypeHelper;
         $this->productHelper     = $productHelperFactory->create(['config' => $config]);
@@ -99,19 +103,19 @@ class Reminder extends AbstractHelper
             if ($orderItem->getProductType() == Configurable::TYPE_CODE) {
                 continue;
             }
-            
+
             $parentItem = $orderItem->getParentItem();
             $productId = $orderItem->getProductId();
-            $product = $mainProduct = $this->productFactory->create()->load($productId);
-            
+            $product = $mainProduct = $this->productRepository->getById($productId, false, $storeId);
+
             $parentProductId = $parentItem ? $parentItem->getProductId() : null;
             if ($parentProductId) {
-                $mainProduct = $this->productFactory->create()->load($parentProductId);
+                $mainProduct = $this->productRepository->getById($parentProductId, false, $storeId);
             }
 
             $product->setStoreId($storeId);
             $mainProduct->setStoreId($storeId);
-            
+
             $variantProduct = $parentItem && $parentItem->getProductType() == Configurable::TYPE_CODE ? $product : null;
             $data = $this->productHelper->getProductFullData($mainProduct, $variantProduct);
 
