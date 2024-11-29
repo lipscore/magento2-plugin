6604f8806094ffed53c8637fd774af71356a3248
AdamLipscore <adam.o@lipscore.com>
2024-11-29 12:45:41 +0100

    Add new locales 2.X (#26)

--- Model/System/Config/Source/Locale.php
+++ Model/System/Config/Source/Locale.php
@@ -15,18 +15,23 @@ class Locale implements OptionSourceInterface
     {
         return [
             ['value' => 'auto', 'label' => __('Auto')],
+            ['value' => 'br',   'label' => __('Brazilian')],
             ['value' => 'cz',   'label' => __('Czech')],
             ['value' => 'dk',   'label' => __('Danish')],
             ['value' => 'nl',   'label' => __('Dutch')],
             ['value' => 'en',   'label' => __('English')],
+            ['value' => 'et',   'label' => __('Estonian')],
             ['value' => 'fi',   'label' => __('Finnish')],
             ['value' => 'fr',   'label' => __('French')],
             ['value' => 'de',   'label' => __('German')],
             ['value' => 'it',   'label' => __('Italian')],
             ['value' => 'ja',   'label' => __('Japanese')],
+            ['value' => 'lv',   'label' => __('Latvian')],
             ['value' => 'no',   'label' => __('Norwegian')],
+            ['value' => 'pl',   'label' => __('Polish')],
             ['value' => 'br',   'label' => __('Portuguese (Brazil)')],
             ['value' => 'ru',   'label' => __('Russian')],
+            ['value' => 'sk',   'label' => __('Slovak')],
             ['value' => 'es',   'label' => __('Spanish')],
             ['value' => 'se',   'label' => __('Swedish')],
         ];
