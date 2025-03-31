078186e717a827e6bacc61bc0f439054c5116d8d
AdamLipscore <adam.o@lipscore.com>
2025-03-31 21:54:15 +0200

    Add new service widget sticky badge 2.X (#41)

--- Block/Product/QA.php
+++ Block/Product/QA.php
@@ -3,7 +3,6 @@
 namespace Lipscore\RatingsReviews\Block\Product;
 
 use Lipscore\RatingsReviews\Block\AbstractBlock;
-use Lipscore\RatingsReviews\Block\Product\QA\Title;
 
 class QA extends AbstractBlock
 {
@@ -11,21 +10,8 @@ class QA extends AbstractBlock
 
     protected function _beforeToHtml()
     {
-        try {
-            $this->setTabTitle();
-        } catch (\Exception $e) {
-            $this->logger->log($e);
-            $this->setTitle(__('Q&A'));
-        }
+        $this->setTitle(__('Q&A'));
         parent::_beforeToHtml();
         return $this;
-    }
-
-    protected function setTabTitle()
-    {
-        $title = $this->getLayout()
-            ->createBlock(Title::class)
-            ->toHtml();
-        $this->setTitle($title);
     }
 }
